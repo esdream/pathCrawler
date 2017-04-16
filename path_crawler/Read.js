72b4fa38263b7fdd60b6cfbb6e38bb7663452dab
esdream <conquerfu@gmail.com>
2017-04-16 13:26:30 +0800

    modify readme_en

--- path_crawler/Read.js
+++ path_crawler/Read.js
@@ -50,7 +50,7 @@ let pathAnalysis = (readFileName, writeFileName) => {
 
 
 // 运行前修改此处路径
-pathAnalysis('./combination_files/combination35.txt', './path_results/path35.txt');
+pathAnalysis('./combination_files/combination1.txt', './path_results/path1.txt');
 
 // 爬取路径方法
 let fetchUrl = (writeStream, elements, callback) => {
